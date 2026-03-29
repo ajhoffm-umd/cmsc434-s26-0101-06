6b40bec77284aa5d749491b1fc29f187758714c6
ajhoffm-umd <ajhoffm@umd.edu>
2026-03-29 15:44:25 -0400

    pantry add sort & delete

--- Phase 2/pantry.js
+++ Phase 2/pantry.js
@@ -13,10 +13,11 @@ defaultKitchenLocations.forEach(location => {
 locationOptions += `<option value="${location}">${location}</option>`;
 });
 
+var itemDeleteFlag = false;
 
 selectElement.innerHTML = locationOptions;
 
-const kitchenItems = [
+var kitchenItems = [
   {
     name: "Rice",
     location: "Pantry",
@@ -49,7 +50,199 @@ function updateListDisplay() {
     kitchenItems.forEach(item => {pushElemListAdd(item);});
 }
 
-// sort buttons
+// actually adds to pantry list on html
+function pushElemListAdd(item) {
+
+    var currentLocation = document.getElementById("listdropdown").value;
+    if (currentLocation !== item.location && currentLocation !== "All") { return }
+
+    // create new element
+    var li = document.createElement("LI");
+    li.classList.add("item-row");
+    
+
+    // add name span with text
+    const name = document.createElement("span");
+    name.textContent = item.name;
+    li.id = item.name;
+
+    // add quantity span if present
+    const quantity = document.createElement("span");
+    if (item.hasOwnProperty("quantity")) {
+        quantity.textContent = item.quantity;
+    } else {
+        quantity.textContent = "--";
+    }
+
+    // add expiration date if present
+    const expiration = document.createElement("span");
+    if (item.hasOwnProperty("expirationDate")) {
+        const today = new Date();
+        const exp = new Date(item.expirationDate);
+        const oneDay = 1000 * 60 * 60 * 24;
+        const daysLeft = Math.ceil((exp.getTime() - today.getTime()) / oneDay);
+
+        expiration.textContent = daysLeft;
+    } else {
+        expiration.textContent = "--";
+    }
+
+    // add hidden delete button
+
+    li.appendChild(name);
+    li.appendChild(quantity);
+    li.appendChild(expiration);
+
+    li.onclick = function() {
+        invListItemOnClick(this);
+    }
+
+    document.getElementById("pantrylist").appendChild(li);
+}
+
+kitchenItems.forEach(item => {pushElemListAdd(item)});
+
+// ITEM DELETE MODE CODE --------------------- //
+
+// html elements
+var deleteQueue = [];
+
+function invListItemOnClick(elem) {
+    if (itemDeleteFlag) {
+
+        var i = deleteQueue.indexOf(elem.id.toLowerCase());
+        console.log(i);
+        if (i >= 0) {
+            console.log("removing " + elem.id + " from queue");
+            deleteQueue.splice(i, 1);
+            elem.style.background = bg_main;
+        } else {
+            console.log("adding " + elem.id + " to queue");
+            deleteQueue.push(elem);
+            elem.style.background = "red";
+        }
+        
+    } 
+}
+
+function toggleListDeleteMode() {
+    
+    itemDeleteFlag = !itemDeleteFlag;
+    if(itemDeleteFlag) {
+        document.getElementById("sortsearch").disabled = true;
+        document.getElementById("sortalpha").disabled = true;
+        document.getElementById("sortexp").disabled = true;
+        document.getElementById("itemdeletemodetoggle").style.background = "red";
+        document.getElementById("itemadd").style.display = "none";
+        document.getElementById("itemdeletecancel").style.display = "block";
+    } else {
+        document.getElementById("sortsearch").disabled = false;
+        document.getElementById("sortalpha").disabled = false;
+        document.getElementById("sortexp").disabled = false;
+        document.getElementById("itemdeletemodetoggle").style.background = "green";
+        if (deleteQueue.length != 0) {
+            deleteQueue.forEach(elem => {
+                console.log("removing " + elem.id);
+                kitchenItems = kitchenItems.filter(item => item.name.toLowerCase() != elem.id.toLowerCase());
+                elem.remove();
+            });
+        }
+        document.getElementById("itemdeletemodetoggle").style.background = "green";
+        document.getElementById("itemadd").style.display = "block";
+        document.getElementById("itemdeletecancel").style.display = "none";
+    }
+}
+
+function itemDeleteCancel() {
+    document.getElementById("sortsearch").disabled = false;
+    document.getElementById("sortalpha").disabled = false;
+    document.getElementById("sortexp").disabled = false;
+    deleteQueue.forEach(elem =>{
+        elem.style.background = bg_main;
+    });
+    deleteQueue = [];
+    document.getElementById("itemdeletemodetoggle").style.background = "green";
+    document.getElementById("itemadd").style.display = "block";
+    document.getElementById("itemdeletecancel").style.display = "none";
+    itemDeleteFlag = false;
+    
+}
+
+
+
+// NEW ITEM FORM ------------------------------ //
+
+// add a list item using the form
+
+function addListItemFormSubmit() {
+
+    var name, location, quantity, expirationDate;
+
+    name = document.getElementById("newlistitemname").value.trim();
+    location = document.getElementById("newlistitemdropdown").value.trim();
+    quantity = document.getElementById("newlistitemquantity").value.trim();
+    expirationDate = document.getElementById("newlistitemdate").value.trim();
+
+    var nameflag = false;
+    kitchenItems.forEach(item => {
+        if (item.name.toLowerCase() == name.toLowerCase()) {
+            document.getElementById("formnamewarning").style.display = "block";
+            nameflag = true;
+            console.log("known");
+        }
+    });
+    if(nameflag){
+        return false;
+    }
+
+    const newItem = {
+        name,
+        location
+    };
+
+    if (quantity !== "") {
+        newItem.quantity = quantity;
+    }
+
+    if (expirationDate !== "") {
+        newItem.expirationDate = expirationDate;
+    }
+
+    kitchenItems.push(newItem);
+
+    pushElemListAdd(newItem);
+
+    closeListAddForm();
+
+    return false; // prevents page refresh
+
+}
+
+// open the add an item form
+
+function openListAddForm() {
+    document.getElementById("listaddform").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+
+    const selectElement = document.getElementById('newlistitemdropdown');
+
+    let locationOptions = '';
+    defaultKitchenLocations.forEach(location => {
+    locationOptions += `<option value="${location}">${location}</option>`;
+    });
+
+    selectElement.innerHTML = locationOptions;
+}
+
+function closeListAddForm() {
+  document.getElementById("listaddform").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("formnamewarning").style.display = "none";
+}
+
+
+// SORT BUTTONS --------------------- //
+
 function sortListItemAlpha() {
     kitchenItems.sort((a, b) => {
         var i = a.name.localeCompare(b.name);
@@ -125,108 +318,3 @@ function searchListItems() {
 
     updateListDisplay();
 }
-
-
-
-// actually adds to pantry list on html
-function pushElemListAdd(item) {
-
-    var currentLocation = document.getElementById("listdropdown").value;
-    if (currentLocation !== item.location && currentLocation !== "All") { return }
-
-    // create new element
-    var li = document.createElement("LI");
-    li.classList.add("item-row");
-
-    const name = document.createElement("span");
-    name.textContent = item.name;
-
-    const quantity = document.createElement("span");
-    if (item.hasOwnProperty("quantity")) {
-        quantity.textContent = item.quantity;
-    } else {
-        quantity.textContent = "--";
-    }
-
-    
-    const expiration = document.createElement("span");
-    if (item.hasOwnProperty("expirationDate")) {
-        const today = new Date();
-        const exp = new Date(item.expirationDate);
-        const oneDay = 1000 * 60 * 60 * 24;
-        const daysLeft = Math.ceil((exp.getTime() - today.getTime()) / oneDay);
-
-        expiration.textContent = daysLeft;
-    } else {
-        expiration.textContent = "--";
-    }
-
-    li.appendChild(name);
-    li.appendChild(quantity);
-    li.appendChild(expiration);
-
-    document.getElementById("pantrylist").appendChild(li);
-}
-
-kitchenItems.forEach(item => {pushElemListAdd(item)});
-
-
-
-
-
-// add a list item using the form
-
-function addListItemFormSubmit() {
-
-    var name, location, quantity, expirationDate;
-
-    name = document.getElementById("newlistitemname").value.trim();
-    location = document.getElementById("newlistitemdropdown").value.trim();
-    quantity = document.getElementById("newlistitemquantity").value.trim();
-    expirationDate = document.getElementById("newlistitemdate").value.trim();
-
-    const newItem = {
-        name,
-        location
-    };
-
-    if (quantity !== "") {
-        newItem.quantity = quantity;
-    }
-
-    if (expirationDate !== "") {
-        newItem.expirationDate = expirationDate;
-    }
-
-    kitchenItems.push(newItem);
-
-    pushElemListAdd(newItem);
-
-    closeListAddForm();
-
-    return false; // prevents page refresh
-
-}
-
-// open the add an item form
-
-function openListAddForm() {
-    document.getElementById("listaddform").style.display = "block";
-    document.getElementById("overlay").style.display = "block";
-
-    const selectElement = document.getElementById('newlistitemdropdown');
-
-    let locationOptions = '';
-    defaultKitchenLocations.forEach(location => {
-    locationOptions += `<option value="${location}">${location}</option>`;
-    });
-
-    selectElement.innerHTML = locationOptions;
-}
-
-function closeListAddForm() {
-  document.getElementById("listaddform").style.display = "none";
-  document.getElementById("overlay").style.display = "none";
-}
-
-
